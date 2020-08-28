8dc198c32273c287e7da8d546603b67c1130a604
2300546456@qq.com <2300546456@qq.com>
2020-08-28 23:19:33 +0800

    Site updated: 2020-08-28 23:19:30

--- js/mogai.js
+++ js/mogai.js
@@ -8,3 +8,13 @@ for (var i = 0; i < menus_item_child.length; i++) {
     menus_expand[i].className += " menus-closed";
 }
 
+//创建对象
+var xiaokang = new xkTool();
+//手机默认栏不打开
+xiaokang.mobileSidebar();
+//Mac代码框绿色按钮放大
+xiaokang.codeFull();
+//点击目录输出锚点
+xiaokang.consoleAnchor();
+//页脚养鱼
+xiaokang.footFish();
