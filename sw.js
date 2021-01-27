346ac8696a9cfc95746ffc81b9ce56bf573551aa
2300546456@qq.com <2300546456@qq.com>
2021-01-27 16:48:37 +0800

    Site updated: 2021-01-27 16:48:34

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","900d93da010e0a6c55350b433e5cd2cc"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
